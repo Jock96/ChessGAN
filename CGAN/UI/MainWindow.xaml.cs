337b68ec27ba99b83cd9f4461a7619f8b50041ea
Unknown <Peter96pvl@gmail.com>
2019-11-06 18:45:49 +0700

    #0001
    Инициализация UI.

---
+++ CGAN/UI/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace UI
+{
+    using System.Windows;
+
+    using UI.ViewModels;
+
+    /// <summary>
+    /// Код-бехайнд главного окна.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Точка входа.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowVM();
+        }
+    }
+}
